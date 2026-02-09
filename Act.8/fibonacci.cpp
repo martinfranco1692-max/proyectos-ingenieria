3a51dab00da19a351820e0793a03a17da3816e5e
Martin Franco Miranda <martin.franco1692@alumnos.udg.mx>
2026-02-08 23:40:00 -0600

    Add files via upload

---
+++ Act.8/fibonacci.cpp
@@ -0,0 +1,75 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int numero, numeroanterior, temp, opcion, limite, i;
+
+    do {
+        cout << "\n1. for \n2. while \n3. do while \n4. salir\nSeleccione: ";
+        cin >> opcion;
+
+        switch(opcion) {
+            case 1:
+                cout << "Ejecutando for\n";
+                cout << "Dame un valor al que quieres llegar?: ";
+                cin >> limite;
+
+                // Reiniciar Fibonacci
+                numero = 0;
+                numeroanterior = 1;
+
+                for (i = 0; numero <= limite; i++) {
+                    cout << numero << endl;
+                    temp = numero;
+                    numero = numero + numeroanterior;
+                    numeroanterior = temp;
+                }
+                break;
+
+            case 2:
+                cout << "Ejecutando while ...\n";
+                cout << "Dame un valor al que quieres llegar: ";
+                cin >> limite;
+
+                // Reiniciar Fibonacci
+                numero = 0;
+                numeroanterior = 1;
+
+                while (numero <= limite) {
+                    cout << numero << endl;
+                    temp = numero;
+                    numero = numero + numeroanterior;
+                    numeroanterior = temp;
+                }
+                break;
+
+            case 3:
+                cout << "Ejecutando do while...\n";
+                cout << "Dame un valor al que quieres llegar: ";
+                cin >> limite;
+
+                // Reiniciar Fibonacci
+                numero = 0;
+                numeroanterior = 1;
+
+                do {
+                    cout << numero << endl;
+                    temp = numero;
+                    numero = numero + numeroanterior;
+                    numeroanterior = temp;
+                } while (numero <= limite);
+                break;
+
+            case 4:
+                cout << "Saliendo...\n";
+                break;
+
+            default:
+                cout << "Opcion invalida.\n";
+        }
+
+    } while(opcion != 4);
+
+    return 0;
+}
